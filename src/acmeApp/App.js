7fc17f0d64fe437e7ff8685521da65d00c66cb2b
adamstruppler <adamstruppler@gmail.com>
2017-10-30 15:48:06 -0600

    homework

--- src/acmeApp/App.js
+++ src/acmeApp/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './index.css'
 
-const Jumbotron = ({title, address}) => {
+const Jumbotron = ({title, street, city, state, zip, phonenumber}) => {
   return (
     <div className='jumbotron'>
       <h1>{title}</h1>
-      <p>{address}</p>
+      <p>{street}</p>
+      <p>{city}</p>
+      <p>{state}</p>
+      <p>{zip}</p>
+      <p>{phonenumber}</p>
     </div>
   )
 }
@@ -23,7 +28,7 @@ const NavigationBar = () => {
     <nav className='nav'>
       <Link icon={'home'} name={'HOME'} />
       <Link icon={'map-o'} name={'ABOUT'} />
-      <Link icon={'motorcycle'} name={'PRODUCTS'} />
+      <Link icon={'product-hunt'} name={'PRODUCTS'} />
       <Link icon={'address-book'} name={'CONTACT'} />
     </nav>
   )
@@ -45,7 +50,11 @@ const App = ({data}) => {
       <NavigationBar />
       <Jumbotron
         title={data.company[0].title}
-        address={data.company[0].street}
+        street={data.company[0].street}
+        city={data.company[0].city}
+        state={data.company[0].state}
+        zip={data.company[0].zip}
+        phonenumber={data.company[0].phoneNumber}
       />
       <CustomerCards
         title={data.customers[0].name}
