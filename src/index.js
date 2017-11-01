40eac2a95a353ac91cf8f343479c5e9e17cca9b4
adamstruppler <adamstruppler@gmail.com>
2017-11-01 13:48:11 -0600

    Monster App

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom'
 // import App from './webApp/App'
 import App from './acmeApp/App'
 import data from './acmeApp/data'
+import MonsterApp from './monsterMash/monsterApp'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App data={data} />, document.getElementById('root'))
+ReactDOM.render(<MonsterApp />, document.getElementById('root'))
+// ReactDOM.render(<App data={data} />, document.getElementById('root'))
 registerServiceWorker()
---
+++ src/monsterMash/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Jumbotron = ({title}) => {
+  return (
+    <div className='jumbotron'>
+      <h1> {title} </h1>
+    </div>
+  )
+}
+
+Jumbotron.propTypes = {
+  title: PropTypes.string.isRequired
+}
+export default Jumbotron
